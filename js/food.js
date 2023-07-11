3617b7ccc480270b7ef7a74e045e2f2ab47e0ffd
Mona Mohamed <111650146+MonaMohamedAbdelaziz2003@users.noreply.github.com>
2023-07-11 05:56:26 +0300

    Add files via upload

--- js/food.js
+++ js/food.js
@@ -18,34 +18,35 @@ if (Modernizr.touch) {
     $(".alert").removeClass("hidden");
 }
 //arw
-var navbar = document.getElementById("nav2");
+var navbar = document.getElementsByClassName("nav1");
 var sticky = navbar.offsetTop;
 var arrow = document.getElementById("arrow");
+console.log(navbar)
 if (window.location.pathname == "/index.html") {
 
-    window.onscroll = function() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("fixed");
-            navbar.style.backgroundColor = "white";
-        } else {
-            navbar.classList.remove("fixed")
-            navbar.style.backgroundColor = "#3bb81500";
-        }
-        if (window.scrollY >= 1200) {
-            arrow.style.opacity = 1;
-        } else {
-            arrow.style.opacity = 0;
-        }
-    }
-    arrow.onclick = function x() {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        }
-        /////////////bootstrap
-    $(document).ready(function() {
-        $(".owl-carousel").owlCarousel();
-    });
+    // window.onscroll = function() {
+    //     if (window.pageYOffset >= sticky) {
+    //         navbar.classList.add("fixed");
+    //         navbar.style.backgroundColor = "white";
+    //     } else {
+    //         navbar.classList.remove("fixed")
+    //         navbar.style.backgroundColor = "#3bb81500";
+    //     }
+    //     if (window.scrollY >= 1200) {
+    //         arrow.style.opacity = 1;
+    //     } else {
+    //         arrow.style.opacity = 0;
+    //     }
+    // }
+    // arrow.onclick = function x() {
+    //         window.scrollTo({ top: 0, behavior: "smooth" });
+    //     }
+    /////////////bootstrap
 
 }
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
 $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
@@ -64,12 +65,12 @@ $('.owl-carousel').owlCarousel({
     })
     /////////////////////menu
 
-function xx() {
-    var div1 = document.getElementById("div1_header");
-
+function xx(x, y) {
+    var div1 = document.getElementById(x);
+    var navbarr = document.getElementById(y);
     div1.classList.toggle("border");
-    navbar.classList.toggle("add_nav2");
-
+    navbarr.classList.toggle("add_nav2");
+    // console.log(div1, "hi", navbarr)
 }
 
 /////////////bootstrap
